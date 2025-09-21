e881a762c3d39cae4dda423824d20867dce4f2d1
Aditya Patra <adityabpatra@gmail.com>
2025-09-20 21:23:55 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -10,31 +10,31 @@ This program performs and analysis of daily average temperatures during the mont
 #include <fstream>
 #include <algorithm>
 #include <numeric>
-#include <array>
+#include <vector>
 using namespace std;
 
 int main() {
-    const int SIZE = 31;
     ifstream fin;
-    array<double, SIZE> numbers;
+    vector<double> numbers;
     string month;
+    double temp;
     fin.open("array_info.txt");
     if (fin.good( )) {
         fin >> month;
-        for(int i = 0; i < SIZE; i++) {
-            fin >> numbers[i];
+        while (fin >> temp) {
+            numbers.push_back(temp);
         }
         // print temperatures
         cout << "Analyzing daily average temperature in month of " << month;
         cout << "\nNumber of days recorded: " << numbers.size();
         cout << "\nAverage temperature per day: ";
-        for(int i = 0; i < SIZE; i++) {
+        for(int i = 0; i < numbers.size(); i++) {
             cout << numbers.at(i) << ", ";
         }
         cout << "\nIncrease in temperature during month: " << numbers.back() - numbers.front();
         cout << "\nMinimum Temperature: " << *min_element(numbers.begin(), numbers.end());
         cout << "\nMaximum Temperature: " << *max_element(numbers.begin(), numbers.end());
-        cout << "\nAverage Temperature: " << accumulate(numbers.begin(), numbers.end(), 0) / numbers.size();
+        cout << "\nAverage Temperature: " << accumulate(numbers.begin(), numbers.end(), 0.0) / numbers.size();
         cout << "\nDays over 15 C: " << count_if(numbers.begin(), numbers.end(), [](int t){ return t >= 15; });
         cout << "\nDays under 15 C: " << count_if(numbers.begin(), numbers.end(), [](int t){ return t < 15; });
         fin.close( );
